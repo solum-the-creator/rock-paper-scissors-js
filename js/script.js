f2e8bcca41326724f600980886a36388c9cd4e92
Andrew Kostukevich <mr.kostukevi4@mail.ru>
2023-01-19 14:36:44 +0300

    Create functions singleRoundOfGame, random and getComputerChoice

--- js/script.js
+++ js/script.js
@@ -1,1 +1,50 @@
 "use strict"
+
+function getComputerChoice() {
+
+    let randomNum = random(3);
+    if(randomNum === 1) return "Rock";
+    if(randomNum === 2) return "Paper";
+    if(randomNum === 3) return "Scissors";
+}
+
+
+function random(number) {
+    return Math.floor(Math.random()*number+1);
+}
+
+function singleRoundOfGame(playerChoice){
+    playerChoice = playerChoice.toLowerCase();
+    let  computerChoice = getComputerChoice().toLowerCase();
+
+    if(playerChoice === computerChoice) return "Tie! No one get score.";
+
+    if(playerChoice === "rock") {
+        if(computerChoice === "paper"){
+            return "You Lose! Paper beats Rock :(";
+        }
+        if(computerChoice === "scissors") {
+            return "You Win! Rock beats Scissors :)";
+        }
+    }
+
+    if(playerChoice === "paper") {
+        if(computerChoice === "rock"){
+            return "You Win! Paper beats Rock :)";
+        }
+        if(computerChoice === "scissors") {
+            return "You Lose! Scissors beats Paper :(";
+        }
+    }
+
+    if(playerChoice === "scissors") {
+        if(computerChoice === "rock"){
+            return "You Lose! Rock beats Scissors :(";
+        }
+        if(computerChoice === "paper") {
+            return "You Win! Scissors beats Paper :)";
+        }
+    }
+}
+
+
